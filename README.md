31f4a3054e5eedf092aca5e73db188dff3ef6f02
Deepak Patidar <deepakpatidar@Deepaks-MacBook-Air.local>
2026-01-18 02:10:02 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# React + Vite
+-Create Vite React app
+-Configure Tailwind CSS
+
+# Features
+-Login/Sign up
+   - Sign In/Sign up free
+   - redirect to browse page
+-Browse
+  -Header
+  -Main movie
+   -Trailer in BackGround
+   -Title & Description
+   -Movie Suggestions
+     -MovieLists + #
+- NetflixGPT
+   - Search Bar
+   - Movie Suggestions
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind is working 🎉
+      </h1>
+    </div>
+  )
+}
